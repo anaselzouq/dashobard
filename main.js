3959d9f2dbd87ee8465b96153a0dd3685a19c4ae
Anas Elzouq <145136810+anaselzouq@users.noreply.github.com>
2024-10-04 18:21:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let closeBtn = document.querySelector("#close-btn");
+let sideMenu = document.querySelector("adside");
+let menuBtn = document.querySelector("#menu-btn");
+let themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+})
